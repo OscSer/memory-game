b542959e04142dceb3390a19ccfc0d70766be143
Oscar Serna <oscr.serna@gmail.com>
2023-04-20 23:24:47 -0500

    Add memory logo component

---
+++ src/components/memory-logo/MemoryLogo.tsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/memory-logo.png';
+import './MemoryLogo.css';
+
+export function MemoryLogo() {
+  return (
+    <div className="memoryLogo">
+      <img src={logo} alt="Memory Logo" className="memoryLogo__img" />
+      Memory
+    </div>
+  );
+}
