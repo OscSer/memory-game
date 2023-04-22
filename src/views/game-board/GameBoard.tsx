a67edbf45763b1362c3da5d3678c5b4731831fbd
Oscar Serna <oscr.serna@gmail.com>
2023-04-22 16:30:23 -0500

    Add alert, counter and restart action in game board

--- src/views/game-board/GameBoard.tsx
+++ src/views/game-board/GameBoard.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { ArrowClockwise, Stars } from 'react-bootstrap-icons';
+import { useNavigate } from 'react-router-dom';
 
 import './GameBoard.css';
 import { useUserState } from '@contexts/user-context/UserContext';
@@ -6,29 +8,60 @@ import { useGetCards } from '@hooks/cards-query/useCardsQuery';
 import { CounterType } from '@models/CounterType';
 import { Loading } from '@components/loading/Loading';
 import { CardGrid } from '@components/card-grid/CardGrid';
+import { CustomAlert } from '@components/alert/CustomAlert';
+import { Counter } from '@components/counter/Counter';
 
 export function GameBoard() {
+  const navigate = useNavigate();
   const user = useUserState();
   const { isLoading, data } = useGetCards(user.numberOfCards);
+  const [alertShow, setAlertShow] = useState(false);
   const [counter, setCounter] = useState<CounterType>({
     successful: 0,
     failed: 0,
   });
 
   useEffect(() => {
-    if (counter.successful === user.numberOfCards) {
-      alert('You won!');
-    }
+    if (counter.successful === user.numberOfCards) setAlertShow(true);
   }, [counter.successful, user.numberOfCards]);
+
+  const restart = useCallback(() => navigate('/'), [navigate]);
+
+  useEffect(() => {
+    if (!user.nickname) restart();
+  }, [restart, user.nickname]);
+
+  const getAlertMessage = () => (
+    <>
+      <Stars />
+      {' Congratulations '}
+      <b>{user.nickname}</b>
+      {'! You have completed the game in '}
+      {counter.failed + counter.successful}
+      {' attempts '}
+      <Stars />
+    </>
+  );
 
   if (isLoading) return <Loading />;
 
   return (
     <div className="gameBoard">
-      <div>{user.nickname}</div>
-      <div>
-        {counter.successful} / {counter.failed}
+      <div className="gameBoard__restart" onClick={restart} aria-hidden="true">
+        <ArrowClockwise /> restart game
       </div>
+
+      <div className="gameBoard__counters">
+        <Counter label="Successful" value={counter.successful} />
+        <Counter label="Failed" value={counter.failed} />
+      </div>
+
+      <CustomAlert
+        show={alertShow}
+        variant="primary"
+        body={getAlertMessage()}
+      />
+
       <CardGrid data={data} setCounter={setCounter} />
     </div>
   );
