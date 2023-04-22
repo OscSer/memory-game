9269d801984bcd1821a3912920fb5aae74bea1ce
Oscar Serna <oscr.serna@gmail.com>
2023-04-22 17:17:30 -0500

    Add default props to duplicated cards in duplicateAndRandomizeCards function

--- src/views/game-board/gameBoardUtils.ts
+++ src/views/game-board/gameBoardUtils.ts
@@ -3,10 +3,25 @@ import { CardType } from '@models/CardType';
 
 export function duplicateAndRandomizeCards(cards?: CardType[]): CardElement[] {
   if (!cards) return [];
+
+  const defaultProps = {
+    show: false,
+    matched: false,
+    selected: false,
+  };
+
   return cards
     .flatMap((item) => [
-      { ...item, key: `${item.uuid}-1`, show: false, matched: false },
-      { ...item, key: `${item.uuid}-2`, show: false, matched: false },
+      {
+        ...item,
+        ...defaultProps,
+        key: `${item.uuid}-1`,
+      },
+      {
+        ...item,
+        ...defaultProps,
+        key: `${item.uuid}-2`,
+      },
     ])
     .sort(() => Math.random() - 0.5);
 }
